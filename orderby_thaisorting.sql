d536395ed15174125e6305595236386129c74639
muzikiii <mame_mameaw@hotmail.com>
2023-02-17 18:16:33 +0700

    Update orderby_thaisorting.sql

--- orderby_thaisorting.sql
+++ orderby_thaisorting.sql
@@ -4,3 +4,7 @@ SELECT province from (
 SELECT distinct province FROM Schema_name.table_name
 ) as prov_name
 order by  province COLLATE "th_TH" asc;
+
+
+
+---> ดูข้อมูลตัวอย่างได้ที่ https://medium.com/@muzikiii/postgresql-order-by-thai-sorting-b80f220f51e5
